2d496df30f380f2ba574eb587cfa79ba1aa90bc1
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-06-28 11:53:38 -0500

    feat: implement login page with GitHub login button and theme provider integration

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,16 +1,26 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { GithubIcon } from "lucide-react";
+
 export default function LoginPage() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-xl">Bienvenido a  Open Mind 🔓🧠</CardTitle>
+        <CardDescription>Iniciar Sesion con:</CardDescription>
+      </CardHeader>
 
-
-
-    return (
-
-        <div>
-            <h1>Hola Login</h1>
-        </div>
-
-
-    )
-
-
-
+      <CardContent>
+        <Button className="w-full" variant="outline">
+          Iniciar Sesion con Github <GithubIcon className="size-4" />
+        </Button>
+      </CardContent>
+    </Card>
+  );
 }
