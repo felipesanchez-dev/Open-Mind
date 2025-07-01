4dba9bca5d6c7310394afcb8a680626d6ec35aa6
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-06-30 21:02:53 -0500

    feat(courses): implement course creation page layout and structure

--- app/admin/courses/create/page.tsx
+++ app/admin/courses/create/page.tsx
@@ -1,5 +1,33 @@
+import { buttonVariants } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
+
 export default function CourseCreationPage() {
     return (
-        <h1>Hola crear curso</h1>
+        <>
+        <div className="flex items-center gap-4">
+            <Link href="/admin/courses" className={buttonVariants({
+                variant: "outline",
+                size: "icon",
+            })}>
+                <ArrowLeft className="size-5" />
+            </Link>
+            <h1 className="text-2xl font-bold">Crear un curso</h1>
+        </div>
+
+            <Card>
+                <CardHeader>
+                    <CardTitle>Información basica del curso</CardTitle>
+                    <CardDescription>Información básica sobre el curso del curso</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    
+                </CardContent>
+
+            </Card>
+
+
+        </>
     )
 }
