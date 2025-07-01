938b3ed5f71a1583e7bbffbd54f9cb218c07a06c
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-06-30 19:43:36 -0500

    feat(admin-layout): refactor layout to accept children and remove unused components

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,14 +1,11 @@
 import { AppSidebar } from "@/components/sidebar/app-sidebar";
-import { ChartAreaInteractive } from "@/components/sidebar/chart-area-interactive";
-import { DataTable } from "@/components/sidebar/data-table";
-import { SectionCards } from "@/components/sidebar/section-cards";
 import { SiteHeader } from "@/components/sidebar/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
-import data from "./data.json";
+import { ReactNode } from "react";
 
-export default function AdminLayout() {
-    return (
-         <SidebarProvider
+export default function AdminLayout({ children }: { children: ReactNode }) {
+  return (
+    <SidebarProvider
       style={
         {
           "--sidebar-width": "calc(var(--spacing) * 72)",
@@ -22,15 +19,11 @@ export default function AdminLayout() {
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-              <SectionCards />
-              <div className="px-4 lg:px-6">
-                <ChartAreaInteractive />
-              </div>
-              <DataTable data={data} />
+              {children}
             </div>
           </div>
         </div>
       </SidebarInset>
     </SidebarProvider>
-    )
+  );
 }
