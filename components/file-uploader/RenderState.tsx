cce23995b853f9029eaa482f4992f8e086bcd2e1
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-07-06 13:22:09 -0500

    feat(uploader): enhance file upload UI with drag-and-drop support and error handling

--- components/file-uploader/RenderState.tsx
+++ components/file-uploader/RenderState.tsx
@@ -1,7 +1,42 @@
+import { CloudUploadIcon, ImageIcon } from "lucide-react";
+import { Button } from "../ui/button";
+import { cn } from "@/lib/utils";
+
 export function RenderEmptyState({ isDragging }: { isDragging: boolean }) {
   return (
     <div className="text-center">
-      <div></div>
+      <div className="flex items-center mx-auto justify-center size-12 rounded-full bg-muted mb-4">
+        <CloudUploadIcon className={cn(
+          "size-6 text-muted-foreground",
+          isDragging && "text-primary"
+        )}/>
+      </div>
+      <p className="text-base font-semibold text-zinc-600">
+        Arrastre y suelte su{" "}
+        <span className="font-bold cursor-pointer text-white">
+          Archivo aqui
+        </span>
+      </p>
+      <Button type="button" className="cursor-pointer mt-4" variant="outline">
+        Seleccionar archivo
+      </Button>
     </div>
   );
+};
+
+export function RenderErrorState() {
+  return (
+    <div className="text-center">
+      <div className="flex items-center mx-auto justify-center size-12 rounded-full bg-muted mb-4">
+        <ImageIcon className={cn(
+          "size-6 text-muted-foreground",
+        )}/>
+      </div>
+      <p className="text-primary font-semibold">Actualizar imagen</p>
+      <p className="text-sm text-muted-foreground">Por favor, intente nuevamente.</p>
+      <Button type="button" className="cursor-pointer mt-4" variant="outline">
+        Seleccionar archivo
+      </Button>
+    </div>
+  )
 }
--- components/file-uploader/Uploader.tsx
+++ components/file-uploader/Uploader.tsx
@@ -1,24 +1,113 @@
 "use client";
-import { useCallback } from "react";
-import { useDropzone } from "react-dropzone";
+import { useCallback, useState } from "react";
+import { FileRejection, useDropzone } from "react-dropzone";
 import { Card, CardContent } from "../ui/card";
 import { cn } from "@/lib/utils";
+import { RenderEmptyState, RenderErrorState } from "./RenderState";
+import { toast } from "sonner";
+import { v4 as uuidv4 } from "uuid";
+
+interface UploaderState {
+  id: string | null;
+  file: File | null;
+  uploading: boolean;
+  progress: number;
+  key?: string;
+  isDeleting: boolean;
+  error: boolean;
+  objectUrl?: string;
+  fileType: "image" | "video";
+}
 
 export function Uploader() {
+  const [fileState, setFileState] = useState<UploaderState>({
+    error: false,
+    file: null,
+    id: null,
+    uploading: false,
+    progress: 0,
+    isDeleting: false,
+    fileType: "image",
+  });
+
+  function uploadFile(file: File) {
+    setFileState((prev) => ({
+      ...prev,
+      uploading: true,
+      progress: 0,
+    }));
+
+    try {
+
+    } catch {
+
+    }
+
+  }
+
   const onDrop = useCallback((acceptedFiles: File[]) => {
-    console.log(acceptedFiles);
+    if (acceptedFiles.length > 0) {
+      const file = acceptedFiles[0];
+
+      setFileState({
+        file: file,
+        uploading: false,
+        progress: 0,
+        objectUrl: URL.createObjectURL(file),
+        error: false,
+        id: uuidv4(),
+        isDeleting: false,
+        fileType: "image",
+      });
+    }
   }, []);
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  function rejectedFiles(fileRejection: FileRejection[]) {
+    if (fileRejection.length) {
+      const tooManyFiles = fileRejection.find(
+        (rejection) => rejection.errors[0].code === "too-many-files"
+      );
+
+      const fileSizeToBig = fileRejection.find(
+        (rejection) => rejection.errors[0].code === "file-too-large"
+      );
+
+      if (fileSizeToBig) {
+        toast.error(
+          "El archivo es demasiado grande, el tamaño máximo permitido es de 10MB"
+        );
+      }
+
+      if (tooManyFiles) {
+        toast.error("Solo se permite un archivo");
+      }
+    }
+  }
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: {
+      "image/*": [],
+    },
+    maxFiles: 1,
+    multiple: false,
+    maxSize: 10 * 1024 * 1024, // 10MB
+    onDropRejected: rejectedFiles,
+  });
 
   return (
-    <Card {...getRootProps()} className={cn(
+    <Card
+      {...getRootProps()}
+      className={cn(
         "relative border-2 border-dashed transition-colors duration-200 ease-in-out w-full h-64",
-        isDragActive ? "border-primary bg-primary/10 border-solid":
-        "border-border hover:border-primary"
-    )}>
+        isDragActive
+          ? "border-primary bg-primary/10 border-solid"
+          : "border-border hover:border-primary"
+      )}
+    >
       <CardContent className="flex items-center justify-center h-full w-full">
         <input {...getInputProps()} />
-        <p>Hola</p>
+        <RenderEmptyState isDragging={isDragActive} />
       </CardContent>
     </Card>
   );
