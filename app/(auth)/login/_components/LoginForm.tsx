abc70a20d63ac30d7368d4fa2ab6b97449f6546f
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-07-10 13:46:21 -0500

    fix(auth): Remove unused parameter 'type' from sendVerificationOTP function

--- app/(auth)/login/_components/LoginForm.tsx
+++ app/(auth)/login/_components/LoginForm.tsx
@@ -14,8 +14,6 @@ import { Separator } from "@/components/ui/separator";
 import { authClient } from "@/lib/auth-client";
 import {
   Mail,
-  Eye,
-  EyeOff,
   Loader,
   GithubIcon,
   Loader2,
@@ -31,12 +29,7 @@ export function LoginForm() {
   const [githubPending, startGithubTransition] = useTransition();
   const [googlePending, startGoogleTransition] = useTransition();
   const [emailPending, startEmailTransition] = useTransition();
-  const [showPassword, setShowPassword] = useState(false);
   const [email, setEmail] = useState("");
-
-  const togglePasswordVisibility = () => {
-    setShowPassword(!showPassword);
-  };
 
   async function signInWithGithub() {
     startGithubTransition(async () => {
